15dd5be677007e9b4d7fcf82787facb3fc7e633a
Łukasz <lukaszgorzelak91@gmail.com>
2022-03-11 09:21:33 +0100

    added calculator feature

--- src/js/fixed.js
+++ src/js/fixed.js
@@ -3,15 +3,12 @@ const logo = document.querySelector('img.logo');
 const items = document.querySelectorAll('.nav__list-item');
 const btnBuy = document.querySelector('.buy');
 const hamburger = document.querySelector('.hamburger');
-const languagesList = document.querySelector('.languages__list');
-// const displayProperty = window.getComputedStyle(hamburger).getPropertyValue('display');
 
 const Fixed = () => {
     header.classList.add("fixed");
     logo.style.marginTop = '20px';
     btnBuy.style.marginTop = '16px';
     hamburger.style.cssText = "bottom: 0; right: 2%";
-    languagesList.style.cssText = "margin-top: 0; margin-left: 0rem";
 
     for (const item of items) {
         item.style.marginTop = '35px';
@@ -24,7 +21,6 @@ const Fixed = () => {
         logo.style.marginTop = '45px';
         btnBuy.style.marginTop = '41px';
         hamburger.style.cssText = "bottom: unset; top: 7.4%; right: 0";
-        languagesList.style.cssText = "margin-top: 6rem; margin-left: 0rem";
         for (const item of items) {
             item.style.marginTop = '60px';
         }
